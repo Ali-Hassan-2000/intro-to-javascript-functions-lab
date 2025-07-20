c25314ef0cc5b35088518d8a60e4d102bb4ea630
Dell <ali.hassan.salman.2000@gmail.com>
2025-07-20 14:14:12 +0300

    All E

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const maxOfTwoNumbers = (x, y) => {
   }
 }
 console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+console.log('===============================================');
 
 /*
 Exercise 2: isAdult()
@@ -26,14 +27,10 @@ Example: isAdult(21) should return 'Adult'.
 
 Complete the exercise in the space below:
 */
-const isAdult = (age) => {
-  if (age >= 18) {
-    return "Adult";
-  } else {
-    return "Minor";
-  }
-}
+const isAdult = (age) => age >= 18 ? 'Adult' : 'Minor';
+
 console.log('Exercise 2 Result:', isAdult(21));
+console.log('===============================================');
 
 /*
 Exercise 3: isCharAVowel()
@@ -48,6 +45,139 @@ Example: isCharAVowel('a') should return true.
 Complete the exercise in the space below:
 */
 
-
+const isCharAVowel = (char) => 'aeiou'.includes(char.toLowerCase());
 
 console.log('Exercise 3 Result:', isCharAVowel("a"));
+console.log('===============================================');
+
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
+const generateEmail = (name, domain) => `${name}@${domain}`;
+
+
+console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+console.log('===============================================');
+
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+
+const greetUser = (name, timeOfDay) => {
+    if (!['morning', 'afternoon', 'evening'].includes(timeOfDay)) {
+        return "Invalid time of day";
+    }
+    return `Good ${timeOfDay}, ${name}!`;
+}
+
+console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
+console.log('===============================================');
+
+/*
+Exercise 6: maxOfThree()
+
+Define a function, maxOfThree. It should accept three numbers 
+and return the largest among them.
+
+Example: maxOfThree(17, 4, 9) should return 17.
+
+Complete the exercise in the space below:
+*/
+
+const maxOfThree = (a, b, c) => a >= b && a >= c ? a : b >= c ? b : c;
+
+console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
+console.log('===============================================');
+
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+
+const calculateTip = (billAmount, tipPercentage) => (billAmount % 1) !== 0 || (tipPercentage % 1) !== 0 ? "Please add a whole number to the function." : billAmount * (tipPercentage / 100);
+
+console.log('Exercise 7 Result:', calculateTip(50, 20));
+console.log('===============================================');
+
+/*
+Exercise 8: convertTemperature()
+
+Write a function named convertTemperature. 
+It takes two arguments: a temperature and a string representing the 
+scale ('C' for Celsius, 'F' for Fahrenheit). 
+Convert the temperature to the other scale.
+
+Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+Complete the exercise in the space below:
+*/
+
+const convertTemperature = (temp, scale) => !['c','f'].includes(scale.toLowerCase()) ? "Invalid scale" : scale.toLowerCase() === 'c' ? (temp * 9/5) + 32 : (temp - 32) * 5/9;
+
+console.log('Exercise 8 Result:', convertTemperature(32, "C"));
+console.log('Exercise 8 Result:', convertTemperature(32, "F"));
+console.log('===============================================');
+
+/*
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+
+const basicCalculator = (n1, n2, op) => {
+
+    op = op.toLowerCase();
+
+    switch (op) {
+        case 'add':
+        return n1 + n2;
+        case 'subtract':
+        return n1 - n2;
+        case 'multiply':
+        return n1 * n2;
+        case 'divide':
+        return n2 !== 0 ? n1 / n2 : "No chance to divide";
+        default:
+        return "Invalid operation";
+    }
+}
+
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "add"));
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "multiply"));
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "divide"));
